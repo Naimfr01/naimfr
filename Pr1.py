c0f0aa0b27259bfc1e41bfbb7791bced05cea0d8
Naimfr01 <92347634+Naimfr01@users.noreply.github.com>
2021-10-12 11:24:55 +0700

    Add files via upload

---
+++ Pr1.py
@@ -0,0 +1,13 @@
+print("\nKONVERSI TEMPERATUR SUHU\n")
+
+celcius = float(input("masukkan suhu ="))
+print("suhu adalah",celcius,"celcius")
+
+reamur = (4/5) * celcius
+print("suhu adalah",reamur,"reamur")
+
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu adalah",fahrenheit,"fahrenheit")
+
+kelvin = (celcius-32)*5/9+273
+print("suhu adalah",kelvin,"kelvin") 
